31b8ae20125cd294321902233e6f22e563e1213b
Alstudd <alstonsoares17@gmail.com>
2024-04-12 14:35:37 +0530

    Added prisma, clerk auth and navbar

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Custom AI ChatBot - Notes",
+};
+
+type Props = {}
+
+const Notes = (props: Props) => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default Notes
